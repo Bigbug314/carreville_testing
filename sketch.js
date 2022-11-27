2d58471e7730980a2ce0cd5701e6d9a4a8e0ec0f
Bigbug314 <bigbug31415@gmail.com>
2022-11-27 13:21:11 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 const size = 20;
 
+const screenSize = 700;
+
 let city;
 
 
@@ -9,30 +11,54 @@ let beach = false;
 
 
 function setup() {
-    createCanvas(700, 750);
+    createCanvas(screenSize + 10, screenSize + 50);
+    strokeWeight(0);
+    textAlign(CORNER);
 
     console.log(width + "   " + height);
 
-    city = new City(width / size, width / size, size, (0, 0, 0), (255, 255, 255), 1);
+    city = new City(screenSize / size, screenSize / size, size, (0, 0, 0), (255, 255, 255), 1);
 }
 
 
 
 function draw() {
     background(80);
-    city.draw(0, 0);
+    city.draw(10, 0);
 
-    textAlign(CORNER);
     textSize(30);
     fill(255, 255, 255)
-    text("Area: " + city.get_area(), 50, 735);
-    text("Perimeter: " + city.get_perimeter(beach), 250, 735);
-    text("Beach: " + beach, 500, 735);
+    text("Area: " + city.get_area(), 60, 735);
+    text("Perimeter: " + city.get_perimeter(beach), 260, 735);
+    text("Beach: ", 510, 735);
+
+    //Beach button
+    if (beach) {
+        fill(0, 255, 0);
+        rect(620, 710, 30, 30);
+    } else {
+        fill(255, 0, 0);
+        rect(620, 710, 30, 30);
+    }
+
+    //Beach
+    if (beach) {
+        fill(220, 220, 0);
+        rect(0, 0, 10, 700);
+    }
+}
+
+
+function mouseClicked() {
+    //Beach button
+    if (mouseX > 610 && mouseX < 640 && mouseY > 710 && mouseY < 740) {
+        beach = !beach;
+    }
 }
 
 
 function mousePressed() {
-    let grid_coord = city.world_to_grid(mouseX, mouseY);
+    let grid_coord = city.world_to_grid(mouseX - 10, mouseY);
 
     if (city.get_square(grid_coord[0], grid_coord[1])) {
         drawing_mode = false;
@@ -44,7 +70,7 @@ function mousePressed() {
 }
 
 function mouseDragged() {
-    let grid_coord = city.world_to_grid(mouseX, mouseY);
+    let grid_coord = city.world_to_grid(mouseX - 10, mouseY);
 
     city.set_square(grid_coord[0], grid_coord[1], drawing_mode);
 }
